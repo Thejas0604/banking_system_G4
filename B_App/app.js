a29a1a5b0db04c5062af24551eb86469fb735105
Thejas0604 <meddepolamact.21@uom.lk>
2023-10-18 11:45:56 +0530

    contact page

--- B_App/app.js
+++ B_App/app.js
@@ -34,6 +34,10 @@ app.get("/about", (req, res) => {
   res.render("about.ejs");
 });
 
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
