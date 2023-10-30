3c7264411145803989c47a45e9d13f8911ffff5d
Nimeth Menuka <nimethm5@gmail.com>
2023-10-30 23:33:09 +0530

    add procedure savings account validate

--- Sql_Database/database.sql
+++ Sql_Database/database.sql
@@ -766,6 +766,37 @@ DELIMITER ;
 --
 -- Dumping routines for database 'banking_system'
 --
+/*!50003 DROP PROCEDURE IF EXISTS `ValidateSavingsAccount` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ValidateSavingsAccount`(IN p_account_no VARCHAR(20), OUT p_is_valid CHAR)
+BEGIN
+    DECLARE v_count INT;
+
+    -- Check if the provided account number exists in the "savings_account" table.
+    SELECT COUNT(*) INTO v_count
+    FROM savings_account
+    WHERE account_no = p_account_no;
+
+    -- If the count is greater than 0, the account is valid; otherwise, it's invalid.
+    IF v_count > 0 THEN
+        SET p_is_valid = "1";
+    ELSE
+        SET p_is_valid = "0";
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -776,4 +807,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-30 23:24:50
+-- Dump completed on 2023-10-30 23:32:28
